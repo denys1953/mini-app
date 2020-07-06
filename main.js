f6163229be57c8ff91d8935091dc631f83a29a87
denys1953 <den25640@gmail.com>
2020-07-06 21:36:36 +0300

    master

---
+++ main.js
@@ -0,0 +1,44 @@
+let app = document.querySelector('#app')
+let res = document.querySelector('#restart')
+let intArr = []
+let arr = []
+res.addEventListener('click', restart)
+for (let i = 1; i <= 25; i++) {
+   intArr.push(i)
+}
+for (let i = 10; i <= 25; i++) {
+   arr.push(i)
+}
+shuffleArray(intArr)
+function arrPaint() {
+   intArr.forEach(e => {
+      let create = document.createElement('div')
+      create.className = 'cell'
+      create.innerHTML = e
+      create.setAttribute('style', RandomStyle())
+      app.appendChild(create)
+   })
+}
+
+function shuffleArray(array) {
+   for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+   }
+}
+function restart() {
+   shuffleArray(intArr)
+   app.innerHTML = null
+   arrPaint()
+}
+arrPaint() 
+
+function RandomInt() {
+   return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function RandomStyle() {
+   return `font-size: ${RandomInt()}px`
+}
